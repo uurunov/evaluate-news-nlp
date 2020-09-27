3581dc551679f248df616b68fac42a573c3bc423
uurunov <ulugbekurunov1997@mail.ru>
2020-09-27 19:57:24 +0500

    fixed spec file

--- __test__/handleSubmit.spec.js
+++ __test__/handleSubmit.spec.js
@@ -10,5 +10,5 @@ describe("Testing the submit functionality", () => {
 		   // Define the expected output, if any, in the form of variables/array
            // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
            // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-           expect(handleSubmit()).not.toBe(" ");
+           expect(handleSubmit).not.toBe(" ");
 })});
